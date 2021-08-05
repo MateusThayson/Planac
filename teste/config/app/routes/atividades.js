29a88dbb3d983237f1f440a1086a5dfd2e256a82
Mateus Thayson <34683332+mateusthaysonufc@users.noreply.github.com>
2021-08-05 20:40:26 -0300

    Delete teste directory

--- teste/config/app/routes/atividades.js
+++
@@ -1,7 +0,0 @@
-const controller = require("../controllers/atividade");
-const controllerAuth = require("../controllers/auth");
-
-module.exports = function(app){
-    app.use("/atividades", controllerAuth.checar);
-    app.post("/atividades", controller.planejarAtividade);
-}
